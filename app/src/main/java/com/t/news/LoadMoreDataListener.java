ddfe5b7491cee16de7dbc4f9fde234750c45b3bd
lanzishou <524398590@qq.com>
2017-07-26 18:10:46 +0800

    null

---
+++ app/src/main/java/com/t/news/LoadMoreDataListener.java
@@ -0,0 +1,9 @@
+package com.t.news;
+
+/**
+ * Created by Administrator on 2017/7/26.
+ */
+
+public interface LoadMoreDataListener {
+	public abstract void loadMoreData();
+}
---
+++ app/src/main/java/com/t/news/RecyclerOnItemClickListener.java
@@ -0,0 +1,8 @@
+package com.t.news;
+
+
+import android.view.View;
+
+public interface RecyclerOnItemClickListener {
+	public abstract void onClick(View view);
+}
