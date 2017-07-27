da1e29d391514241a879d6d638305a492fad2994
lanzishou <524398590@qq.com>
2017-07-27 18:04:24 +0800

    null

--- app/src/main/java/com/t/news/NewsPreview.java
+++ app/src/main/java/com/t/news/NewsPreview.java
@@ -3,10 +3,13 @@ package com.t.news;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -29,12 +32,18 @@ import okhttp3.Callback;
 import okhttp3.Response;
 
 public class NewsPreview extends Fragment {
-
+	public SwipeRefreshLayout swipeRefresh;
 	private List<Data> newsList = new ArrayList<>();
 	private List<Data> moreData = new ArrayList<>();
 	private News news;
+	private News renews;
+	private LinearLayoutManager LayoutManager;
+	private RecyclerView RecyclerView;
 	private PreviewAdapter adapter;
-	int mPagePosition = 10;
+	private int mPagePosition = 10;
+	private int tabIndex;
+	private SharedPreferences prefs;
+	private String newsString;
 	private Handler handler = new Handler();
 	private String path[] = new String[]{"http://v.juhe.cn/toutiao/index?type=top&key=073b888449d480372f360d7bf4c37ecf",
 			"http://v.juhe.cn/toutiao/index?type=shehui&key=073b888449d480372f360d7bf4c37ecf",
@@ -47,60 +56,98 @@ public class NewsPreview extends Fragment {
 			"http://v.juhe.cn/toutiao/index?type=caijing&key=073b888449d480372f360d7bf4c37ecf",
 			"http://v.juhe.cn/toutiao/index?type=shishang&key=073b888449d480372f360d7bf4c37ecf",
 	};
-	RecyclerView RecyclerView;
-
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.news_preview, container, false);
 		RecyclerView = (RecyclerView) view.findViewById(R.id.news_preview);
+		swipeRefresh = (SwipeRefreshLayout) view.findViewById(R.id.sfl);
+		swipeRefresh.setColorSchemeResources(R.color.colorPrimary);
+		LayoutManager = new LinearLayoutManager(getContext());
+		LayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+		RecyclerView.setLayoutManager(LayoutManager);
+		adapter = new PreviewAdapter(getContext(), RecyclerView);
+		RecyclerView.setAdapter(adapter);
+		Bundle bundle = getArguments();
+		tabIndex = bundle.getInt("tabIndex");
+		prefs = PreferenceManager.getDefaultSharedPreferences(getContext());
+		newsString = prefs.getString(path[tabIndex],null);
+		if (newsString != null){
+			news = Utility.handleNewsResponse(newsString);
+			showNews();
+		}else {
+			new MyTask().requestNews(getActivity(), path[tabIndex]);
+			showNews();
+		}
 		return view;
 	}
 
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		Bundle bundle = getArguments();
-		int tabIndex = bundle.getInt("tabIndex");
-		switch (tabIndex) {
-			case 0:
-				new MyTask().requestNews(getActivity(), path[0]);
-				break;
-			case 1:
-				new MyTask().requestNews(getActivity(), path[1]);
-				break;
-			case 2:
-				new MyTask().requestNews(getActivity(), path[2]);
-				break;
-			case 3:
-				new MyTask().requestNews(getActivity(), path[3]);
-				break;
-			case 4:
-				new MyTask().requestNews(getActivity(), path[4]);
-				break;
-			case 5:
-				new MyTask().requestNews(getActivity(), path[5]);
-				break;
-			case 6:
-				new MyTask().requestNews(getActivity(), path[6]);
-				break;
-			case 7:
-				new MyTask().requestNews(getActivity(), path[7]);
-				break;
-			case 8:
-				new MyTask().requestNews(getActivity(), path[8]);
-				break;
-			case 9:
-				new MyTask().requestNews(getActivity(), path[9]);
-				break;
+		swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+			@Override
+			public void onRefresh() {
+				Utility.sendOkHttpRequest(path[tabIndex], new Callback() {
+					@Override
+					public void onFailure(Call call, IOException e) {
+						e.printStackTrace();
+						getActivity().runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								Toast.makeText(getActivity(), "获取新闻信息失败", Toast.LENGTH_SHORT).show();
+								swipeRefresh.setRefreshing(false);
+							}
+						});
+					}
 
-			default:
-				break;
-		}
+					@Override
+					public void onResponse(Call call, Response response) throws IOException {
+						String responseText = response.body().string();
+						SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+						editor.putString(path[tabIndex],responseText);
+						editor.apply();
+						news = Utility.handleNewsResponse(responseText);
+						newsString = prefs.getString(path[tabIndex],null);
+						getActivity().runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								new MyTask().requestNews(getActivity(), path[tabIndex]);
+								swipeRefresh.setRefreshing(false);
+								showNews();
+							}
+						});
+					}
+				});
+			}
+		});
+		adapter.setOnMoreDataLoadListener(new LoadMoreDataListener() {
+			@Override
+			public void loadMoreData() {
+				//加入null值此时adapter会判断item的type
+				if (mPagePosition < news.result.dataList.size()) {
+					newsList.add(null);
+					adapter.notifyDataSetChanged();
+					handler.postDelayed(new Runnable() {
+						@Override
+						public void run() {
+							//移除刷新的progressBar
+							newsList.remove(newsList.size() - 1);
+							adapter.notifyDataSetChanged();
+							initMoreData();
+							newsList.addAll(moreData);
+							adapter.notifyDataSetChanged();
+							adapter.setLoaded();
+						}
+					}, 2000);
+				}
+			}
+		});
+
 	}
 
 	private class MyTask {
-		private void requestNews(final Activity activity, String url) {
+		private void requestNews(final Activity activity,final String url) {
 
 			Utility.sendOkHttpRequest(url, new Callback() {
 				@Override
@@ -122,37 +169,9 @@ public class NewsPreview extends Fragment {
 						@Override
 						public void run() {
 							if (news != null && "成功的返回".equals(news.reason)) {
-								LinearLayoutManager LayoutManager = new LinearLayoutManager(getContext());
-								LayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-								RecyclerView.setLayoutManager(LayoutManager);
-								adapter = new PreviewAdapter(getContext(), RecyclerView);
-								RecyclerView.setAdapter(adapter);
-								for (int i = 0; i < 10; i++) {
-									newsList.add(news.result.dataList.get(i));
-								}
-								adapter.setData(newsList);
-								adapter.setOnMoreDataLoadListener(new LoadMoreDataListener() {
-									@Override
-									public void loadMoreData() {
-										//加入null值此时adapter会判断item的type
-										if (mPagePosition < news.result.dataList.size()) {
-											newsList.add(null);
-											adapter.notifyDataSetChanged();
-											handler.postDelayed(new Runnable() {
-												@Override
-												public void run() {
-													//移除刷新的progressBar
-													newsList.remove(newsList.size() - 1);
-													adapter.notifyDataSetChanged();
-													initMoreData();
-													newsList.addAll(moreData);
-													adapter.notifyDataSetChanged();
-													adapter.setLoaded();
-												}
-											}, 2000);
-										}
-									}
-								});
+								SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+								editor.putString(url,responseText);
+								editor.apply();
 							} else {
 								Toast.makeText(activity, "获取新闻信息失败", Toast.LENGTH_SHORT).show();
 							}
@@ -162,6 +181,15 @@ public class NewsPreview extends Fragment {
 			});
 
 		}
+	}
+
+	private void showNews(){
+		newsList.clear();
+		for (int i = 0; i < 10; i++) {
+			newsList.add(news.result.dataList.get(i));
+		}
+		adapter.setData(newsList);
+		adapter.notifyDataSetChanged();
 	}
 
 	private void initMoreData() {
