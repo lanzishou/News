c83bd36a6a4786849f40c44c3360502e14874323
lanzishou <524398590@qq.com>
2017-07-31 18:10:23 +0800

    null

--- app/src/main/java/com/t/news/NewsActivity.java
+++ app/src/main/java/com/t/news/NewsActivity.java
@@ -67,8 +67,6 @@ public class NewsActivity extends AppCompatActivity {
 
 			}
 		});
-		viewPager.setOffscreenPageLimit(0);
-
 	}
 	private void initFragment() {
 		fragmentList = new ArrayList<Fragment>();
